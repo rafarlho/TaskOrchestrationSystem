d562c032e9d83b0cc0c848c43122f92424976b6c
Rafael Carvalho <raffacarvalho2000@gmail.com>
2025-04-26 13:11:16 +0100

    Fixed layout for -netCore 9

--- TOSApi/Program.cs
+++ TOSApi/Program.cs
@@ -1,21 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
+builder.Services.AddRazorPages();
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
-
+app.UseSwagger();
+app.UseSwaggerUI(options => 
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
 app.UseAuthorization();
-app.MapControllers();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
 app.Run();
