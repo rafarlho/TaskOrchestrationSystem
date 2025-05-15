d4bbbcea550c41e1ab4feb175d1dc0c9accc792a
Rafael Carvalho <raffacarvalho2000@gmail.com>
2025-05-15 08:13:37 +0100

    Implemented early basic logics

--- DbDocs/TOS-sql-query.sql
+++ DbDocs/TOS-sql-query.sql
@@ -1,4 +1,4 @@
--- Criação da base de dados (se quiseres fazer isso manualmente, podes saltar esta parte)
+-- Criação da base de dados
 IF DB_ID('TOSDatabase') IS NULL
     CREATE DATABASE TOSDatabase;
 GO
@@ -6,10 +6,10 @@ GO
 USE TOSDatabase;
 GO
 
--- Apagar schema se já existir (cuidado, apaga todas as tabelas dentro!)
+-- Apagar schema se já existir
 IF EXISTS (SELECT * FROM sys.schemas WHERE name = 'TOSDbSchema')
 BEGIN
-    EXEC sp_executesql N'DROP SCHEMA TOSDbSchema CASCADE'; -- funciona no Azure SQL MI, senão usa drop table individual
+    EXEC('DROP SCHEMA TOSDbSchema CASCADE'); -- Nota: "CASCADE" só funciona em Azure SQL Managed Instance
 END
 GO
 
@@ -17,140 +17,140 @@ GO
 CREATE SCHEMA TOSDbSchema;
 GO
 
--- Apagar tabelas se existirem (por segurança)
-DROP TABLE IF EXISTS TOSDbSchema.[task];
-DROP TABLE IF EXISTS TOSDbSchema.[production_task];
-DROP TABLE IF EXISTS TOSDbSchema.[production_station_member];
-DROP TABLE IF EXISTS TOSDbSchema.[production_station];
-DROP TABLE IF EXISTS TOSDbSchema.[organization_member];
-DROP TABLE IF EXISTS TOSDbSchema.[organization];
-DROP TABLE IF EXISTS TOSDbSchema.[user];
-DROP TABLE IF EXISTS TOSDbSchema.[task_type];
-DROP TABLE IF EXISTS TOSDbSchema.[priority];
-DROP TABLE IF EXISTS TOSDbSchema.[status];
-DROP TABLE IF EXISTS TOSDbSchema.[role];
+-- Apagar tabelas se existirem
+DROP TABLE IF EXISTS TOSDbSchema.[Task];
+DROP TABLE IF EXISTS TOSDbSchema.[ProductionTask];
+DROP TABLE IF EXISTS TOSDbSchema.[ProductionStationMember];
+DROP TABLE IF EXISTS TOSDbSchema.[ProductionStation];
+DROP TABLE IF EXISTS TOSDbSchema.[OrganizationMember];
+DROP TABLE IF EXISTS TOSDbSchema.[Organization];
+DROP TABLE IF EXISTS TOSDbSchema.[User];
+DROP TABLE IF EXISTS TOSDbSchema.[TaskType];
+DROP TABLE IF EXISTS TOSDbSchema.[Priority];
+DROP TABLE IF EXISTS TOSDbSchema.[Status];
+DROP TABLE IF EXISTS TOSDbSchema.[Role];
 GO
 
--- Tabelas base
-CREATE TABLE TOSDbSchema.[user] (
-  [id] INT PRIMARY KEY,
-  [full_name] NVARCHAR(255),
-  [username] NVARCHAR(255),
-  [email] NVARCHAR(255),
-  [created_at] DATETIME2,
-  [latest_login] DATETIME2,
-  [db_created_on] DATETIME2,
-  [row_version] ROWVERSION,
-  [db_status] BIT
+-- Tabelas
+
+CREATE TABLE TOSDbSchema.[User] (
+  [Id] INT IDENTITY(1,1) PRIMARY KEY,
+  [FullName] NVARCHAR(255),
+  [Username] NVARCHAR(255) NOT NULL,
+  [Email] NVARCHAR(255) NOT NULL,
+  [LatestLogin] DATETIME2 ,
+  [DbCreatedOn] DATETIME2 NOT NULL,
+  [RowVersion] ROWVERSION NOT NULL,
+  [DbStatus] BIT NOT NULL
 );
 GO
 
-CREATE TABLE TOSDbSchema.[role] (
-  [id] INT PRIMARY KEY,
-  [role] NVARCHAR(255)
+CREATE TABLE TOSDbSchema.[Role] (
+  [Id] INT IDENTITY(1,1) PRIMARY KEY,
+  [Role] NVARCHAR(255) NOT NULL
 );
 GO
 
-CREATE TABLE TOSDbSchema.[organization] (
-  [id] INT PRIMARY KEY,
-  [name] NVARCHAR(255),
-  [description] NVARCHAR(255),
-  [location] NVARCHAR(255),
-  [owner_id] INT,
-  [db_created_on] DATETIME2,
-  [row_version] ROWVERSION,
-  [db_status] BIT,
-  FOREIGN KEY ([owner_id]) REFERENCES TOSDbSchema.[user]([id])
+CREATE TABLE TOSDbSchema.[Organization] (
+  [Id] INT IDENTITY(1,1) PRIMARY KEY,
+  [Name] NVARCHAR(255) NOT NULL,
+  [Description] NVARCHAR(255) NOT NULL,
+  [Location] NVARCHAR(255) NOT NULL,
+  [OwnerId] INT NOT NULL,
+  [DbCreatedOn] DATETIME2 NOT NULL,
+  [RowVersion] ROWVERSION NOT NULL,
+  [DbStatus] BIT NOT NULL,
+  FOREIGN KEY ([OwnerId]) REFERENCES TOSDbSchema.[User]([Id])
 );
 GO
 
-CREATE TABLE TOSDbSchema.[organization_member] (
-  [id] INT PRIMARY KEY,
-  [organization_id] INT,
-  [user_id] INT,
-  [role_id] INT,
-  FOREIGN KEY ([organization_id]) REFERENCES TOSDbSchema.[organization]([id]),
-  FOREIGN KEY ([user_id]) REFERENCES TOSDbSchema.[user]([id]),
-  FOREIGN KEY ([role_id]) REFERENCES TOSDbSchema.[role]([id])
+CREATE TABLE TOSDbSchema.[OrganizationMember] (
+  [Id] INT IDENTITY(1,1) PRIMARY KEY,
+  [OrganizationId] INT NOT NULL,
+  [UserId] INT NOT NULL,
+  [RoleId] INT NOT NULL,
+  FOREIGN KEY ([OrganizationId]) REFERENCES TOSDbSchema.[Organization]([Id]),
+  FOREIGN KEY ([UserId]) REFERENCES TOSDbSchema.[User]([Id]),
+  FOREIGN KEY ([RoleId]) REFERENCES TOSDbSchema.[Role]([Id])
 );
 GO
 
-CREATE TABLE TOSDbSchema.[production_station] (
-  [id] INT PRIMARY KEY,
-  [organization_id] INT,
-  [name] NVARCHAR(255),
-  [description] NVARCHAR(255),
-  FOREIGN KEY ([organization_id]) REFERENCES TOSDbSchema.[organization]([id])
+CREATE TABLE TOSDbSchema.[ProductionStation] (
+  [Id] INT IDENTITY(1,1) PRIMARY KEY,
+  [OrganizationId] INT NOT NULL,
+  [Name] NVARCHAR(255) NOT NULL,
+  [Description] NVARCHAR(255),
+  FOREIGN KEY ([OrganizationId]) REFERENCES TOSDbSchema.[Organization]([Id])
 );
 GO
 
-CREATE TABLE TOSDbSchema.[production_station_member] (
-  [id] INT PRIMARY KEY,
-  [production_station_id] INT,
-  [organization_member_id] INT,
-  FOREIGN KEY ([production_station_id]) REFERENCES TOSDbSchema.[production_station]([id]),
-  FOREIGN KEY ([organization_member_id]) REFERENCES TOSDbSchema.[organization_member]([id])
+CREATE TABLE TOSDbSchema.[ProductionStationMember] (
+  [Id] INT IDENTITY(1,1) PRIMARY KEY,
+  [ProductionStationId] INT NOT NULL,
+  [OrganizationMemberId] INT NOT NULL,
+  FOREIGN KEY ([ProductionStationId]) REFERENCES TOSDbSchema.[ProductionStation]([Id]),
+  FOREIGN KEY ([OrganizationMemberId]) REFERENCES TOSDbSchema.[OrganizationMember]([Id])
 );
 GO
 
-CREATE TABLE TOSDbSchema.[priority] (
-  [id] INT PRIMARY KEY,
-  [priority] NVARCHAR(255)
+CREATE TABLE TOSDbSchema.[Priority] (
+  [Id] INT IDENTITY(1,1) PRIMARY KEY,
+  [Priority] NVARCHAR(255) NOT NULL
 );
 GO
 
-CREATE TABLE TOSDbSchema.[status] (
-  [id] INT PRIMARY KEY,
-  [status] NVARCHAR(255)
+CREATE TABLE TOSDbSchema.[Status] (
+  [Id] INT IDENTITY(1,1) PRIMARY KEY,
+  [Status] NVARCHAR(255) NOT NULL
 );
 GO
 
-CREATE TABLE TOSDbSchema.[task_type] (
-  [id] INT PRIMARY KEY,
-  [type] NVARCHAR(255)
+CREATE TABLE TOSDbSchema.[TaskType] (
+  [Id] INT IDENTITY(1,1) PRIMARY KEY,
+  [Type] NVARCHAR(255) NOT NULL
 );
 GO
 
-CREATE TABLE TOSDbSchema.[production_task] (
-  [id] INT PRIMARY KEY,
-  [title] NVARCHAR(255),
-  [description] NVARCHAR(255),
-  [create_by_id] INT,
-  [organization_id] INT,
-  [started_at] DATETIME2,
-  [finished_at] DATETIME2,
-  [status_id] INT,
-  [priority_id] INT,
-  [db_created_on] DATETIME2,
-  [row_version] ROWVERSION,
-  [db_status] BIT,
-  FOREIGN KEY ([create_by_id]) REFERENCES TOSDbSchema.[organization_member]([id]),
-  FOREIGN KEY ([organization_id]) REFERENCES TOSDbSchema.[organization]([id]),
-  FOREIGN KEY ([status_id]) REFERENCES TOSDbSchema.[status]([id]),
-  FOREIGN KEY ([priority_id]) REFERENCES TOSDbSchema.[priority]([id])
+CREATE TABLE TOSDbSchema.[ProductionTask] (
+  [Id] INT IDENTITY(1,1) PRIMARY KEY,
+  [Title] NVARCHAR(255) NOT NULL,
+  [Description] NVARCHAR(255),
+  [CreatedById] INT NOT NULL,
+  [OrganizationId] INT NOT NULL,
+  [StartedOn] DATETIME2 NOT NULL,
+  [FinishedOn] DATETIME2 NOT NULL,
+  [StatusId] INT NOT NULL,
+  [PriorityId] INT NOT NULL,
+  [DbCreatedOn] DATETIME2 NOT NULL,
+  [RowVersion] ROWVERSION NOT NULL,
+  [DbStatus] BIT NOT NULL,
+  FOREIGN KEY ([CreatedById]) REFERENCES TOSDbSchema.[OrganizationMember]([Id]),
+  FOREIGN KEY ([OrganizationId]) REFERENCES TOSDbSchema.[Organization]([Id]),
+  FOREIGN KEY ([StatusId]) REFERENCES TOSDbSchema.[Status]([Id]),
+  FOREIGN KEY ([PriorityId]) REFERENCES TOSDbSchema.[Priority]([Id])
 );
 GO
 
-CREATE TABLE TOSDbSchema.[task] (
-  [id] INT PRIMARY KEY,
-  [production_task_id] INT,
-  [title] NVARCHAR(255),
-  [description] NVARCHAR(255),
-  [task_type_id] INT,
-  [production_station_id] INT,
-  [production_station_member_id] INT,
-  [scheduled_to] DATETIME2,
-  [started_on] DATETIME2,
-  [finished_on] DATETIME2,
-  [estimated_duration] INT,
-  [status_id] INT,
-  [db_created_on] DATETIME2,
-  [row_version] ROWVERSION,
-  [db_status] BIT,
-  FOREIGN KEY ([production_task_id]) REFERENCES TOSDbSchema.[production_task]([id]),
-  FOREIGN KEY ([task_type_id]) REFERENCES TOSDbSchema.[task_type]([id]),
-  FOREIGN KEY ([production_station_id]) REFERENCES TOSDbSchema.[production_station]([id]),
-  FOREIGN KEY ([production_station_member_id]) REFERENCES TOSDbSchema.[production_station_member]([id]),
-  FOREIGN KEY ([status_id]) REFERENCES TOSDbSchema.[status]([id])
+CREATE TABLE TOSDbSchema.[Task] (
+  [Id] INT IDENTITY(1,1) PRIMARY KEY,
+  [ProductionTaskId] INT NOT NULL,
+  [Title] NVARCHAR(255) NOT NULL,
+  [Description] NVARCHAR(255),
+  [TaskTypeId] INT NOT NULL,
+  [ProductionStationId] INT NOT NULL,
+  [ProductionStationMemberId] INT NOT NULL,
+  [ScheduledTo] DATETIME2 NOT NULL,
+  [StartedOn] DATETIME2,
+  [FinishedOn] DATETIME2,
+  [EstimatedDuration] INT,
+  [StatusId] INT NOT NULL,
+  [DbCreatedOn] DATETIME2 NOT NULL,
+  [RowVersion] ROWVERSION NOT NULL,
+  [DbStatus] BIT NOT NULL,
+  FOREIGN KEY ([ProductionTaskId]) REFERENCES TOSDbSchema.[ProductionTask]([Id]),
+  FOREIGN KEY ([TaskTypeId]) REFERENCES TOSDbSchema.[TaskType]([Id]),
+  FOREIGN KEY ([ProductionStationId]) REFERENCES TOSDbSchema.[ProductionStation]([Id]),
+  FOREIGN KEY ([ProductionStationMemberId]) REFERENCES TOSDbSchema.[ProductionStationMember]([Id]),
+  FOREIGN KEY ([StatusId]) REFERENCES TOSDbSchema.[Status]([Id])
 );
 GO
