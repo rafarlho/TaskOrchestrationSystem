a48e7ac9aba5cc36ebffbb2de7f8898474d570a3
Rafael Carvalho <raffacarvalho2000@gmail.com>
2025-06-11 22:03:42 +0100

    Implemented basic theming

--- TaskOrchestrationSystemUI/src/app/common/sidenav/sidenav.component.ts
+++ TaskOrchestrationSystemUI/src/app/common/sidenav/sidenav.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { ToolbarComponent } from "../toolbar/toolbar.component";
 
 @Component({
   selector: 'tos-sidenav',
   imports: [
-    MatSidenavModule, 
-    MatButtonModule
-  ],
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    ToolbarComponent,
+    MatDividerModule
+],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
+  isCollapsed = false;
 
-}
+  navItems = [
+    { icon: 'home', label: 'Início' },
+    { icon: 'person', label: 'Perfil' },
+    { icon: 'settings', label: 'Definições' },
+    { icon: 'logout', label: 'Sair' }
+  ];
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+} 
